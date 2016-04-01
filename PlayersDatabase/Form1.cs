97afd8343132e46dee97b4c871f1c599aec02e25
emerfan <emerfanning@gmail.com>
2016-04-01 13:26:39 +0100

    Insert a Player Form Working

--- PlayersDatabase/Form1.cs
+++ PlayersDatabase/Form1.cs
@@ -7,14 +7,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace PlayersDatabase
 {
     public partial class Form1 : Form
     {
+        SqlConnection conn = new SqlConnection("Data Source=lugh4.it.nuigalway.ie;Persist Security Info=True;User ID=msdb2367;Password=msdb2367EM");
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double maxSpeed = Convert.ToDouble(textBox3.Text);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Players (First_Name, Last_Name, Age, Height, RunningDistance, MaxSpeed) VALUES(' " + textBox1.Text + " ',' " + textBox2.Text + " ',' " + dateTimePicker1.Value + "',' " + numericUpDown2.Value + "',' " + numericUpDown3.Value + "',' " + maxSpeed + "')");
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery();
+            conn.Close();
         }
     }
 }
